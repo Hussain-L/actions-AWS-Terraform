2d6f70554725d77ec6c26eafe335c7e88f15ff22
Hussain J Lokhandwala <65801800+Hussain-L@users.noreply.github.com>
2022-11-16 12:29:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "myFirstInstance" {
   ami           = "ami-0b9064170e32bde34"
   instance_type = "t2.micro"
   tags= {
-    Name = github-actions
+    Name = "github-actions"
   }
 }
